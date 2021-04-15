9c2e3b2b9ade5fe928599c2c29879eebe4fb379c
soare23 <toma.mihai.sorin23@gmail.com>
2021-04-15 16:53:03 +0300

    working in passing down function to close modal to child component

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientService } from '../../services/patient.service';
 import { Patient } from '../../models/Patient';
 
 @Component({
@@ -8,59 +9,31 @@ import { Patient } from '../../models/Patient';
 })
 export class MainPageComponent implements OnInit {
   patients: Array<Patient>;
+  showUpdateModal: boolean;
+  patientToBeUpdated: Patient;
 
-  constructor() {
-    this.patients = [
-      {
-        id: 1,
-        firstName: 'Garfield',
-        lastName: "O'Keefe",
-        dateOfBirth: '2021-03-26T18:42:44.156Z',
-        gender: 'M',
-        socialNumber: 1940245786123,
-        phoneNumber: '743.655.4845',
-      },
-      {
-        id: 2,
-        firstName: 'Waldo',
-        lastName: 'Prohaska',
-        dateOfBirth: '2020-07-10T20:45:21.591Z',
-        gender: 'M',
-        socialNumber: 1920245786913,
-        phoneNumber: '(518) 073-3113',
-      },
-      {
-        id: 3,
-        firstName: 'Merritt',
-        lastName: 'Pollich',
-        dateOfBirth: '2020-12-05T19:27:19.184Z',
-        gender: 'F',
-        socialNumber: 1960245756923,
-        phoneNumber: '(636) 275-1224',
-      },
-      {
-        id: 4,
-        firstName: 'Mariam',
-        lastName: 'Reinger',
-        dateOfBirth: '2020-04-29T15:28:27.613Z',
-        gender: 'F',
-        socialNumber: 1910245686923,
-        phoneNumber: '730-120-8788',
-      },
-    ];
+  constructor(private patientService: PatientService) {}
+
+  ngOnInit(): void {
+    this.patientService.getPatients().subscribe((patients) => {
+      this.patients = patients;
+    });
   }
 
-  ngOnInit(): void {}
+  handleModal() {
+    this.showUpdateModal = !this.showUpdateModal;
+  }
 
   add() {
     console.log('add new patient');
   }
 
-  remove() {
-    console.log('patient removed from table');
+  remove(patient: Patient) {
+    console.log(`patient with id ${patient.id} has been removed`);
   }
 
-  update() {
-    console.log('patient updated');
+  openUpdateModal(patient: Patient) {
+    this.handleModal();
+    this.patientToBeUpdated = patient;
   }
 }
